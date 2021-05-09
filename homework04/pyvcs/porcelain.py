91924d3be7d70be040fef97c1556fddda2ffbcf5
anastasiablokhina <71010324+anastasiablokhina@users.noreply.github.com>
2021-05-09 19:53:45 +0300

    Update porcelain.py

--- homework04/pyvcs/porcelain.py
+++ homework04/pyvcs/porcelain.py
@@ -10,14 +10,52 @@ from pyvcs.tree import commit_tree, write_tree
 
 def add(gitdir: pathlib.Path, paths: tp.List[pathlib.Path]) -> None:
     # PUT YOUR CODE HERE
-    ...
+    for path in paths:
+        if not path.is_dir():
+            update_index(gitdir, [path], write=True)
+        else:
+            add(gitdir, list(path.glob("*")))
 
 
 def commit(gitdir: pathlib.Path, message: str, author: tp.Optional[str] = None) -> str:
     # PUT YOUR CODE HERE
-    ...
+    tree = write_tree(gitdir, read_index(gitdir), str(gitdir.parent))
+    parent_commit = resolve_head(gitdir)
+    commit_hash = commit_tree(gitdir, tree, message, parent_commit, author)
+    return commit_hash
 
 
 def checkout(gitdir: pathlib.Path, obj_name: str) -> None:
     # PUT YOUR CODE HERE
-    ...
+    for entry in read_index(gitdir):
+        if pathlib.Path(entry.name).exists():
+            os.remove(entry.name)
+    commit_data = commit_parse(read_object(obj_name, gitdir)[1])
+    doing = True
+    while doing:
+        trees: tp.List[tp.Tuple[tp.List[tp.Tuple[int, str, str]], pathlib.Path]] = [
+            (read_tree(read_object(commit_data["tree"], gitdir)[1]), gitdir.parent)
+        ]
+        while trees:
+            tree_content, tree_path = trees.pop()
+            for file_data in tree_content:
+                fmt, data = read_object(file_data[2], gitdir)
+                if fmt != "tree":
+                    if not (tree_path / file_data[1]).exists():
+                        with (tree_path / file_data[1]).open("wb") as file:
+                            file.write(data)
+                            (tree_path / file_data[1]).chmod(int(str(file_data[0]), 8))
+                else:
+                    if not (tree_path / file_data[1]).exists():
+                        (tree_path / file_data[1]).mkdir()
+                    trees.append((read_tree(data), tree_path / file_data[1]))
+        if "parent" in commit_data:
+            commit_data = commit_parse((read_object(commit_data["parent"], gitdir)[1]))
+        else:
+            doing = not doing
+    for dir in gitdir.parent.glob("*"):
+        if dir.is_dir() and dir != gitdir:
+            try:
+                os.removedirs(dir)
+            except OSError:
+                continue
